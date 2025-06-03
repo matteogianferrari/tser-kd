f468ac426decb877ae66b00132e01d1d3affb650
matteogianferrari <mine.matt88@gmail.com>
2025-06-03 18:46:39 +0200

    Added CutOut and MetricMeter classes.

---
+++ tser-kd/eval/__init__.py
@@ -0,0 +1,11 @@
+"""Eval Package.
+
+Modules:
+    metric_meter: Defines the MetricMeter class used to tracks a streaming metric using observed values.
+"""
+
+from .metric_meter import MetricMeter
+
+__all__ = [
+    "MetricMeter",
+]
