b26867823fa7ae1652c47b483866b33f6299e2a9
matteogianferrari <mine.matt88@gmail.com>
2025-06-09 15:50:46 +0200

    Added plotting functions.

--- tser_kd/utils/__init__.py
+++ tser_kd/utils/__init__.py
@@ -3,12 +3,16 @@
 Modules:
     util: Defines functions used during the experiments.
     metric_meter: Defines the 'MetricMeter' class used to tracks a streaming metric using observed values.
+    spikes: 
 """
 
 from .util import setup_seed
+from .spikes import plot_raster_over_channels, plot_spike_train_over_channels
 from .metric_meter import MetricMeter
 
 __all__ = [
     "MetricMeter",
     "setup_seed",
+    "plot_spike_train_over_channels",
+    "plot_raster_over_channels"
 ]
